12c72c7de2164d71d2792682e0957592c03779ee
shiyonggang <shiyonggang@ytsense.cn>
2023-03-23 09:48:16 +0800

    1

--- yxk/yolo/a.py
+++ yxk/yolo/a.py
@@ -11,12 +11,12 @@ import os      # 导入os文件操作模块
 import shutil  # 导入shutil高级文件操作模块
 
 
-root =r"E:\workspace\20230320" # 图片和标签在同一个文件夹中
+root =r"E:\workspace\20230323" # 图片和标签在同一个文件夹中
 if not os.path.exists(root):
     os.makedirs(root)
 
 #源文件夹路径
-path = r"E:\workspace\datasets\3-19\Z15"
+path = r"E:\workspace\datasets\3-21\Z14"
 folders= os.listdir(path)
 for folder in folders:
     dir = path + '\\' +  str(folder)
